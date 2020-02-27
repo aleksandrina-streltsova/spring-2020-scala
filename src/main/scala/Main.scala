1b9af6bc5e03a6a535ac4e0e49c582551f614e39
FadeevaKate <rediska0123@gmail.com>
2020-02-27 17:02:03 +0300

    add bot

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,63 @@
-class Main {
+import cats.instances.future._
+import cats.syntax.functor._
+import com.bot4s.telegram.api.RequestHandler
+import com.bot4s.telegram.api.declarative.Commands
+import com.bot4s.telegram.clients.FutureSttpClient
+import com.bot4s.telegram.future.{Polling, TelegramBot}
+import com.softwaremill.sttp.SttpBackendOptions
+import com.softwaremill.sttp.okhttp.OkHttpFutureBackend
 
+import scala.collection.mutable
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContext, Future}
+
+class BotStarter(override val client: RequestHandler[Future]) extends TelegramBot
+  with Polling
+  with Commands[Future] {
+  val users: mutable.MutableList[Int] = mutable.MutableList[Int]()
+  val messages: mutable.MutableList[(Int, String)] = mutable.MutableList[(Int, String)]()
+
+  onCommand("/start") { implicit msg =>
+    msg.from match {
+      case None => reply("Ты кто")
+      case Some(usr) => users += usr.id
+    }
+
+    reply("You've been registered!").void
+  }
+
+  onCommand("/users") { implicit msg =>
+    reply(users.mkString(", ")).void
+  }
+
+  onCommand("/send") { implicit msg =>
+    withArgs { args =>
+      val id = if (args.isEmpty) ??? else args(0).toInt
+      val message = if (args.size < 2) ??? else args(1)
+      messages += id -> message
+      reply("Sended").void
+    }
+  }
+
+  onCommand("/check") { implicit msg =>
+    val id : Int = msg.from match {
+      case None => ???
+      case Some(usr) => usr.id
+    }
+    reply(messages.filter(_._1 == id).map(_._2).mkString(", ")).void
+  }
+
+}
+
+object BotStarter {
+  def main(args: Array[String]): Unit = {
+    implicit val ec: ExecutionContext = ExecutionContext.global
+    implicit val backend = OkHttpFutureBackend(
+      SttpBackendOptions.Default.socksProxy("ps8yglk.ddns.net", 11999)
+    )
+
+    val token = "1079914748:AAFa1jyE21HbWSQCcVoa0rMG0Awaeje6kPs"
+    val bot = new BotStarter(new FutureSttpClient(token))
+    Await.result(bot.run(), Duration.Inf)
+  }
 }
